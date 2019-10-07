005fdff96b5b008a696bab3e03fde3c28aee0458
wan230114 <1170101471@qq.com>
2019-10-07 18:05:31 +0800

    Update pywget.py

--- pywget.py
+++ pywget.py
@@ -3,8 +3,8 @@
 # @Email:  chenjun2049@foxmail.com
 # @Qmail:  1170101471@qq.com
 # @Date:   2019-09-08 17:24:02
-# @Last Modified by:   11701
-# @Last Modified time: 2019-09-14 13:12:53
+# @Last Modified by:   JUN
+# @Last Modified time: 2019-10-07 18:03:30
 
 import argparse
 import sys
@@ -36,22 +36,27 @@ class pywget:
     '''download file function'''
 
     def __init__(self, config={}):
-        self._block = int(config['block'] if 'block' in config else 1024)
-        self._proxy = config['proxy']
-        self._sock = self.__server_Connect__(config['proxy']) if config['proxy'] else False
+        self._url = config['url']
+        self._block = int(config.get('block', 1024))
+        self._headers = config.get(
+            'headers', {"User-Agent": "Wget/1.12 (cygwin)", "Accept": "*/*"})
+        self._proxy = config.get('proxy', None)
+        self._sock = self.__server_Connect__(self._proxy) if self._proxy else False
         self._size = 0  # 记录断点位置字节数
         self._size_recved = 0  # 接收的字节数
         self._size_total = 0  # 需要下载的文件总字节数
-        self.url = config['url']
         self.filename = config.get('filename', None)
         self.force = config.get('force', False)
 
-    def __remove_nonchars__(self, name):
+    def __remove_nonchars__(self, path_name):
         '''clean name'''
-        newname, n = re.subn(r'[\\\/\:\*\?\"\<\>\|]', '', name)
+        name = os.path.basename(path_name)
+        dirname = os.path.abspath(os.path.dirname(path_name))
+        newname, n = re.subn('[\\\/\:\*\?\"\<\>\|]', '', name)
         if n:
-            print('file renamed: %s --> %s' % (name, newname))
-        return name
+            print('file renamed: %s --> %s' % (name, newname), n)
+        os.makedirs(dirname, exist_ok=True)
+        return os.sep.join([dirname, name])
 
     def __touch__(self, filename):
         '''clean file'''
@@ -60,11 +65,8 @@ class pywget:
 
     def __support_continue__(self, url):
         '''test url'''
-        headers = {
-            'Range': 'bytes=0-'
-        }
         try:
-            r = requests.head(url, headers=headers)
+            r = requests.head(url, headers=self._headers)
             crange = r.headers['content-range']
             self._size_total = int(re.match(r'^bytes 0-\d+/(\d+)$', crange).group(1))
             return True
@@ -85,9 +87,11 @@ class pywget:
             print('[stat]下载不支持断点续传，已重置下载文件')
             self.__touch__(tmp_filename)
             self.__touch__(local_filename)
-        headers_range = "bytes=%d-" % (self._size if self._size == 0
-                                       else self._size - 1)
-        return {'Range': headers_range}
+        if self._size != 0:
+            headers_range = "bytes=%d-" % (self._size - 1)
+            # headers_range = "bytes=%d-" % (self._size if self._size == 0
+            #                                else self._size - 1)
+            self._headers.update(headers_range)
 
     def __getsize__(self, size):
         '''get size'''
@@ -149,15 +153,14 @@ class pywget:
         s.send(header_json)  # 4.发送报文
         s.send(msg)  # 5.发送真实的数据
 
-    def __do_recv__(self, headers):
+    def __do_recv__(self):
         '''down'''
-        url = self.url
         print('[using proxy server]')
         # 发送请求开始信号，辨别身份
         self._sock.send(b'[send-url]')
         # 0) 发送网址信息
-        print('[send-url]%s[%s]' % (url, getpass.getuser()))
-        self.__mysend__(('[send-url]%s[%s]' % (url, getpass.getuser())).encode())
+        print('[send-url]%s[%s]' % (self._url, getpass.getuser()))
+        self.__mysend__(('[send-url]%s[%s]' % (self._url, getpass.getuser())).encode())
         print(time.strftime("[%Y-%m-%d %H:%M:%S, Content Success]",
                             time.localtime()))
         print('正在排队...')
@@ -165,9 +168,8 @@ class pywget:
         # 1) 接收断点续传信息和开始信息
         msg = self.__recv_size__(8)
         if int(msg[0]):
-            headers_range = self.__support_continue_do__(True)
+            self.__support_continue_do__(True)
             # print(headers_range)
-            headers.update(headers_range)
         if msg[1:] == b'[start]':
             print('排队结束, 下载中...')
         else:
@@ -176,7 +178,7 @@ class pywget:
             sys.exit(1)
 
         # 2) 发送网页请求头
-        self.__mysend__(json.dumps(headers).encode('utf-8'))
+        self.__mysend__(json.dumps(self._headers).encode('utf-8'))
 
         # 3) 接收文件总长度
         self._size_total = int(self.__myrecv__())
@@ -220,7 +222,7 @@ class pywget:
         # 1) 文件准备
         finished = False
         print('Download start:')
-        url, filename, force, block = self.url, self.filename, self.force, self._block
+        url, filename, force, block = self._url, self.filename, self.force, self._block
         if not filename:
             filename = self.__remove_nonchars__(url.split('/')[-1])
         local_filename = self.__remove_nonchars__(filename)
@@ -233,7 +235,6 @@ class pywget:
             self.__touch__(tmp_filename)
         self.local_filename, self.tmp_filename = local_filename, tmp_filename
 
-        headers = {"User-Agent": "Wget/1.12 (cygwin)", "Accept": "*/*"}
         if os.path.exists(local_filename) and not os.path.exists(tmp_filename):
             if not force:
                 print('下载文件已存在，%s' % local_filename)
@@ -249,13 +250,11 @@ class pywget:
         # 2) 请求开始
         try:
             if not self._sock:
-                headers_range = self.__support_continue_do__(self.__support_continue__(url))
-                headers.update(headers_range)
-                print(headers)
-                r = requests.get(self.url, stream=True, verify=False, headers=headers)
+                self.__support_continue_do__(self.__support_continue__(url))
+                r = requests.get(self._url, stream=True, verify=False, headers=self._headers)
                 iters = r.iter_content(chunk_size=block)
             else:
-                self.__do_recv__(headers)
+                self.__do_recv__()
                 iters = self.__do_recv2__()
             start_t = time.time()
             if self._size_total > 0:
@@ -287,7 +286,7 @@ class pywget:
                 os.remove(tmp_filename)
                 spend = time.time() - start_t
                 speed_tmp = self.__getsize__(self._size2 - self._size)
-                speed = float(speed_tmp[:-1]) / spend
+                speed = float(speed_tmp[:-1]) / (spend if spend else 1)
                 sys.stdout.write(
                     '\nDownload Finished!\nTotal Time: %.3fs, Download Speed: %.3f%s/s\n' % (spend, speed, speed_tmp[-1]))
                 sys.stdout.flush()
@@ -305,6 +304,12 @@ def main():
     kwargs = fargv()
     # kwargs['url'] = 'http://118.89.194.65/genome.gff3.idx'
     pywget(kwargs).download()
+    # pywget({'url': 'http://cd15-c120-1.play.bokecc.com/flvs/cb/QxEZF/hIZrEKBTdq-2.pcf?t=1570436774&key=C98D110A66E8B92F34D54244D2676754&tpl=10&tpt=111',
+    #         'headers': {"Referer": "http://www.tmooc.cn/player/index.shtml?courseId=5DF587D41C7C40759DEAF911C5C7B188",
+    #                     "Sec-Fetch-Mode": "no-cors",
+    #                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36"},
+    #         'filename': 'test.pcf',
+    #         'force': True}).download()
 
 
 if __name__ == '__main__':
